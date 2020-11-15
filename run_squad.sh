604b55baeb7cab9ab9ec1b94a0ab214ca8bd07c1
Aopolin-Lv <aopolin.ii@gmail.com>
2020-11-15 23:41:11 +0800

    Update: modify some functions and add new python file related to squad

--- run_squad.sh
+++ run_squad.sh
@@ -1,16 +1,16 @@
 export SQUAD_DIR=./SQuAD2.0/dataset
 
-python Code/run_squad_huggingface.py \
+python Code/run_squad.py \
   --model_typ bert \
   --model_name_or_path bert-base-uncased \
   --do_train \
+  --version_2_with_negative \
   --do_eval \
   --do_lower_case \
   --train_file $SQUAD_DIR/train-v2.0.json \
   --predict_file $SQUAD_DIR/dev-v2.0.json \
   --per_gpu_train_batch_size 12 \
   --learning_rate 3e-5 \
-  --version_2_with_negative \
   --no_cuda \
   --num_train_epochs 2.0 \
   --max_seq_length 384 \
